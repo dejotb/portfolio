97b5957198f9619e71d9a3c1f3693994d8172db0
dejotb <dejotb@gmail.com>
2022-05-21 22:44:22 +0200

    styling nav move on click

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 
 // Navigation
 
+const hamburger = document.querySelector('.hamburger');
+const navWrapper = document.querySelector('.nav__list--wrapper');
 
-let hamburger = document.querySelector('.hamburger').getAttribute('aria-expanded');
 
-console.log(hamburger);
+hamburger.addEventListener('click', function(e) {
+
+    const hamburgerState = e.currentTarget.getAttribute('aria-expanded');
+
+    if (hamburgerState === 'false') {
+        console.log(navWrapper);
+        navWrapper.style.transform = 'translate(0)';
+    }
+});
+
+// let hamburger = document.querySelector('.hamburger').getAttribute('aria-expanded');
+
+// console.log(hamburger);
 
 
 
