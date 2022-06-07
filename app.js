3817c82f8613070ab6423018247cece4a9d54272
dejotb <dejotb@gmail.com>
2022-06-07 21:29:57 +0200

    added local storage to btn icon change

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const header = document.querySelector('.header');
 const layout = document.querySelector('.layout');
 const settingsControlPanel = document.querySelector('.settings__control');
 const btnTheme = document.querySelector('.btn--control-theme');
+const btnSound = document.querySelector('.btn--control-sound');
+const headerImage = document.querySelector('.header__image');
+const headerImageText = document.querySelector('.header__image--text');
+const btn = document.querySelectorAll('.btn');
+const nav = document.querySelector('.nav');
 
 hamburger.addEventListener('click', function(e) {
 
@@ -63,25 +68,96 @@ btnSettings.addEventListener('click', function() {
 
 // Sounds
 
+const handleSound = function() {
+
+    const enableSound = () => {
+
+    }
+}
 
 
-const btn = document.querySelectorAll('.btn');
-const nav = document.querySelector('.nav');
 
 const playSound = function(soundName) {
     const audio = new Audio(`sound/${soundName}.mp3`);
     audio.play();
 }
 
-const switchControlBtnState = function(e, state1, state2) {
-    let themeState = e.target.querySelector('[data-title]').getAttribute('data-title');
-    if (themeState === `${state2}`) {
-        e.target.querySelector('[data-title]').setAttribute('data-title', `${state1}`);
+
+
+// Settings control
+
+const switchControlBtnIconState = function(btnType, state1, state2) {
+    let state = btnType.querySelector('[data-title]').getAttribute('data-title');
+    if (state === `${state2}`) {
+        btnType.querySelector('[data-title]').setAttribute('data-title', `${state1}`);
+
     }
     else {
-        e.target.querySelector('[data-title]').setAttribute('data-title', `${state2}`);
+        btnType.querySelector('[data-title]').setAttribute('data-title', `${state2}`);
     }
 }
+
+
+const handleTheme = function() {
+    let lightMode = localStorage.getItem('lightMode');
+    // let themeIconState = localStorage.getItem('themeIconState');
+
+    const enableLightMode = () => {
+        // add the class lightMode to the layout container
+        layout.classList.add('lightMode');
+
+
+        // update lightMode in the localStorage
+        localStorage.setItem('lightMode', 'enabled');
+    }
+
+    const disableLightMode = () => {
+        // add the class lightMode to the layout container
+        layout.classList.remove('lightMode');
+
+        // update lightMode in the localStorage
+        localStorage.setItem('lightMode', null);
+    }
+
+
+
+    if (lightMode === 'enabled') {
+        enableLightMode();
+        switchControlBtnIconState(btnTheme, 'dark_mode', 'sunny');
+    }
+
+    btnTheme.addEventListener('click', (e) => {
+        lightMode = localStorage.getItem('lightMode');
+
+        if (lightMode !== 'enabled') {
+            enableLightMode();
+            playSound('click4');
+            switchControlBtnIconState(btnTheme, 'dark_mode', 'sunny');
+        } else {
+            disableLightMode()
+            playSound('click1');
+            switchControlBtnIconState(btnTheme, 'dark_mode', 'sunny');
+
+        }
+
+    })
+
+
+    // let soundMute = localStorage.getItem('soundMute');
+
+    // const disableSound = () => {
+
+    // }
+}
+
+handleTheme()
+
+
+
+
+
+
+
 
 
 nav.addEventListener('click', function(e) {
@@ -108,24 +184,23 @@ nav.addEventListener('click', function(e) {
     }
 
     if (e.target.closest('.btn--control-theme')) {
-        switchControlBtnState(e, 'dark_mode', 'sunny')
-        playSound('click4');
+        // switchControlBtnState(e, 'dark_mode', 'sunny')
+        // playSound('click4');
     }
 
     if (e.target.closest('.btn--control-sound')) {
-        switchControlBtnState(e, 'volume_off', 'volume_up')
+        // switchControlBtnState(e, 'volume_off', 'volume_up')
+        switchControlBtnIconState(btnSound, 'volume_off', 'volume_up');
         playSound('click3');
     }
 
 
 })
 
+
 // Animation of header image text on click
 
 header.addEventListener('click', function(e) {
-    const headerImage = document.querySelector('.header__image');
-    const headerImageText = document.querySelector('.header__image--text');
-
     if (e.target.closest('.header__image')) {
         headerImage.style.position = 'relative';
         headerImageText.classList.remove('hidden');
@@ -136,51 +211,3 @@ header.addEventListener('click', function(e) {
         playSound('hello');
     }
 })
-
-
-
-
-
-const handleTheme = function() {
-    let lightMode = localStorage.getItem('lightMode');
-
-    const enableLightMode = () => {
-        // add the class lightMode to the layout container
-        layout.classList.add('lightMode');
-
-        // update lightMode in the localStorage
-        localStorage.setItem('lightMode', 'enabled');
-    }
-
-    const disableLightMode = () => {
-        // add the class lightMode to the layout container
-        layout.classList.remove('lightMode');
-
-        // update lightMode in the localStorage
-        localStorage.setItem('lightMode', null);
-    }
-
-    if (lightMode === 'enabled') {
-        enableLightMode();
-    }
-
-    btnTheme.addEventListener('click', (e) => {
-        lightMode = localStorage.getItem('lightMode');
-
-        if (lightMode !== 'enabled') {
-            enableLightMode();
-        } else {
-            disableLightMode()
-        }
-
-    })
-}
-
-handleTheme()
-
-
-let soundMute = localStorage.getItem('soundMute');
-
-const disableSound = () => {
-
-}
