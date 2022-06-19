931640c838ee4cfb06feb8077dd0c80819358184
dejotb <dejotb@gmail.com>
2022-06-19 22:45:19 +0200

    contact form back to thank you page

--- app.js
+++ app.js
@@ -234,23 +234,3 @@ surfer.addEventListener('click', () => {
 
 
 
-
-
-// Send email
-
-
-    fetch("https://formsubmit.co/ajax/dejotb1@gmail.com", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-        },
-        body: JSON.stringify({
-            name: "FormSubmit",
-            message: "I'm from Devro LABS"
-        })
-    })
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.log(error));
-
